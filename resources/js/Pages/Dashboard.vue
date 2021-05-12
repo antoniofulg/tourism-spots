a5f54cb323e9931244813f48175f7b6998b1ded1
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-12 00:18:26 -0300

    feature: setup search request

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,14 +1,43 @@
 <template>
-  <h1>Olá!</h1>
-  <blockquote>{{ title }}</blockquote>
+  <layout>
+    <state-list
+      :states="stateList"
+      @searchStates="(search) => onSubmit(search)"
+    />
+  </layout>
 </template>
 
 <script>
+import Layout from '../shared/Layout'
+import StateList from '../components/states/List'
+import StatesService from '../services/states-service'
+
 export default {
+  name: 'Dashboard',
   props: {
-    title: String,
+    states: Object,
+    search: String,
   },
-};
+  data: () => ({
+    searchStates: [],
+  }),
+  computed: {
+    stateList() {
+      return this.searchStates.data || this.states.data
+    },
+  },
+  components: { Layout, StateList },
+  methods: {
+    async onSubmit(search) {
+      try {
+        const { states } = (await StatesService.search({ search })).data
+        this.searchStates = states
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
