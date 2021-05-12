6a13f16a66b60e269a348385127065b4f8a0f4cc
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-12 00:17:19 -0300

    feature: setup api routes

---
+++ app/Http/Controllers/Api/StateController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\State;
+
+class StateController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    $search = $request->get('search');
+
+    $query = State::query();
+
+    if ($search) {
+      $query->where('name', 'like', '%'. $search .'%')->orWhere('abbreviation', 'like', '%' . $search . '%');
+    }
+
+    $states = $query->paginate(8);
+
+    return response()->json([
+      'states' => $states
+    ]);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+      //
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+      //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+      //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+      //
+  }
+}
