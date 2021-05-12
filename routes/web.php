7a571d3538ab1ae2a5e806ef1abeeddb71ff5e7e
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-12 00:17:52 -0300

    fix: added inertia import

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StateController;
 
-use Inertia\Inertia;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
