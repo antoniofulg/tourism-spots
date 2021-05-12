36fe20d696505b23aaa598fe14c3c78df457ea8c
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-12 00:18:13 -0300

    feature: created layout

---
+++ resources/js/shared/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="bg-gray-200">
+    <main class="container mx-auto pt-4">
+      <slot></slot>
+    </main>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+}
+</script>
+
+<style></style>
