c1349b82606394947f566ab62f9b9d62e8a6ef9b
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-13 23:47:55 -0300

    feature: created city controller

---
+++ app/Http/Controllers/Api/CityController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\City;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Request $request)
+  {
+    $state_id = $request->get('state_id');
+    $search = $request->get('search');
+
+    $query = City::query()->where('state_id', '=', $state_id);
+
+    if ($search) {
+      $query->andWhere('name', 'like', '%'. $search .'%');
+    }
+
+    $cities = $query->paginate(8);
+
+    return response()->json([
+      'cities' => $cities
+    ]);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+      //
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+      //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+      //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+      //
+  }
+}
