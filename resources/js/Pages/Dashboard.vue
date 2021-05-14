0cae53d359ed93635d28a7283cb9e8a4805c6665
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-13 23:56:08 -0300

    feature: setup states and cities lists

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,16 +1,40 @@
 <template>
-  <layout>
-    <state-list
-      :states="stateList"
-      @searchStates="(search) => onSubmit(search)"
+  <layout class="h-screen">
+    <div class="flex flex-col lg:flex-row">
+      <div class="flex-1 mx-8">
+        <states-list
+          :states="statesList"
+          :onSearchState="searchState"
+          :onDeleteState="openDeleteStateDialog"
+          :onSetState="setState"
+          :onCreateState="openCreateStateDialog"
+        />
+      </div>
+      <div v-if="citiesList.length" class="flex-1 mx-8">
+        <cities-list
+          :cities="citiesList"
+          :onSearchState="searchState"
+          :onDeleteState="openDeleteStateDialog"
+          :onSetState="setState"
+          :onCreateState="openCreateStateDialog"
+        />
+      </div>
+    </div>
+    <delete-state-dialog
+      v-if="deleteTargetState !== null"
+      :state="deleteTargetState"
+      :onClose="(deleteTargetState = null)"
     />
   </layout>
 </template>
 
 <script>
-import Layout from '../shared/Layout'
-import StateList from '../components/states/List'
+import Layout from '../components/shared/Layout'
+import StatesList from '../components/states/List'
+import CitiesList from '../components/cities/List'
+import DeleteStateDialog from '../components/states/DeleteDialog'
 import StatesService from '../services/states-service'
+import CitiesService from '../services/cities-service'
 
 export default {
   name: 'Dashboard',
@@ -18,17 +42,23 @@ export default {
     states: Object,
     search: String,
   },
+  components: { Layout, CitiesList, StatesList, DeleteStateDialog },
   data: () => ({
     searchStates: [],
+    cities: [],
+    deleteTargetState: null,
+    searchTargetStateId: null,
   }),
   computed: {
-    stateList() {
+    statesList() {
       return this.searchStates.data || this.states.data
     },
+    citiesList() {
+      return this.cities?.data || []
+    },
   },
-  components: { Layout, StateList },
   methods: {
-    async onSubmit(search) {
+    async searchState(search = '') {
       try {
         const { states } = (await StatesService.search({ search })).data
         this.searchStates = states
@@ -36,8 +66,34 @@ export default {
         console.log(error)
       }
     },
+    openCreateStateDialog() {
+      this.createStateDialog != this.createStateDialog
+    },
+    openDeleteStateDialog(state) {
+      this.deleteTargetState = state
+    },
+    async deleteState(id) {
+      try {
+        const { states } = (await StatesService.delete({ id })).data
+        this.searchStates = states
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async searchCities(state_id, search = '') {
+      try {
+        const { cities } = (await CitiesService.search({ state_id, search }))
+          .data
+        this.cities = cities
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    setState(id) {
+      this.searchTargetStateId = id
+      if (id) this.searchCities(id)
+      else this.cities = []
+    },
   },
 }
 </script>
-
-<style lang="scss" scoped></style>
