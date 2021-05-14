47c59de65041e031cc22658ac91ec92db841189c
Antonio Fulgêncio <antoniofulg@gmail.com>
2021-05-13 23:47:28 -0300

    feature: published cities index route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StateController;
+use App\Http\Controllers\Api\CityController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('states', [StateController::class, 'index']);
+Route::get('cities', [CityController::class, 'index']);
